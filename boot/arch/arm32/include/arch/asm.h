a390ea51fb6add705540d1d7a5f7bb989a44f2b1
Jiří Zárevúcky <zarevucky.jiri@gmail.com>
2018-03-12 19:41:41 +0100

    todo

--- boot/arch/arm32/include/arch/asm.h
+++ boot/arch/arm32/include/arch/asm.h
@@ -42,14 +42,15 @@
 extern pte_level0_section_t boot_pt[PTL0_ENTRIES];
 extern void *boot_stack;
 
+extern void bootstrap(void);
+
 /** Jump to the kernel entry point.
  *
  * @param entry    Kernel entry point.
  * @param bootinfo Structure holding information about loaded tasks.
  *
  */
-extern void jump_to_kernel(void *entry, void *bootinfo)
-    __attribute__((noreturn));
+extern _Noreturn void jump_to_kernel(void *entry, void *bootinfo);
 
 #endif
 
