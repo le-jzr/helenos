291d7df270836dd75f85b0377d5a702b3fe96896
Jiří Zárevúcky <jiri.zarevucky@nic.cz>
2018-06-19 20:27:10 +0200

    Improve the ability to debug futexes and asserts.

--- uspace/lib/c/include/futex.h
+++ uspace/lib/c/include/futex.h
@@ -39,67 +39,27 @@
 #include <errno.h>
 #include <libc.h>
 
+#define FUTEX_DEBUG 1
+
+#if FUTEX_DEBUG
+#include <fibril.h>
+#include <fibril_private.h>
+//#include <io/kio.h>
+//#define __FUTEX_DPRINTF(...) kio_printf(__VA_ARGS__)
+#define __FUTEX_DPRINTF(...) ((void)0)
+#endif
+
 typedef struct futex {
 	atomic_t val;
 #ifdef FUTEX_UPGRADABLE
 	int upgraded;
+#elif FUTEX_DEBUG
+	_Atomic void *owner;
 #endif
 } futex_t;
 
 
 extern void futex_initialize(futex_t *futex, int value);
-
-#ifdef FUTEX_UPGRADABLE
-#include <rcu.h>
-
-#define FUTEX_INITIALIZE(val) {{ (val) }, 0}
-
-#define futex_lock(fut) \
-({ \
-	rcu_read_lock(); \
-	(fut)->upgraded = rcu_access(_upgrade_futexes); \
-	if ((fut)->upgraded) \
-		(void) futex_down((fut)); \
-})
-
-#define futex_trylock(fut) \
-({ \
-	rcu_read_lock(); \
-	int _upgraded = rcu_access(_upgrade_futexes); \
-	if (_upgraded) { \
-		int _acquired = futex_trydown((fut)); \
-		if (!_acquired) { \
-			rcu_read_unlock(); \
-		} else { \
-			(fut)->upgraded = true; \
-		} \
-		_acquired; \
-	} else { \
-		(fut)->upgraded = false; \
-		1; \
-	} \
-})
-
-#define futex_unlock(fut) \
-({ \
-	if ((fut)->upgraded) \
-		(void) futex_up((fut)); \
-	rcu_read_unlock(); \
-})
-
-extern int _upgrade_futexes;
-
-extern void futex_upgrade_all_and_wait(void);
-
-#else
-
-#define FUTEX_INITIALIZE(val) {{ (val) }}
-
-#define futex_lock(fut)     (void) futex_down((fut))
-#define futex_trylock(fut)  futex_trydown((fut))
-#define futex_unlock(fut)   (void) futex_up((fut))
-
-#endif
 
 #define FUTEX_INITIALIZER     FUTEX_INITIALIZE(1)
 
@@ -149,6 +109,155 @@ static inline errno_t futex_up(futex_t *futex)
 
 	return EOK;
 }
+
+#ifdef FUTEX_UPGRADABLE
+#include <rcu.h>
+
+#define FUTEX_INITIALIZE(val) {{ (val) }, 0}
+
+#define futex_lock(fut) \
+({ \
+	rcu_read_lock(); \
+	(fut)->upgraded = rcu_access(_upgrade_futexes); \
+	if ((fut)->upgraded) \
+		(void) futex_down((fut)); \
+})
+
+#define futex_trylock(fut) \
+({ \
+	rcu_read_lock(); \
+	int _upgraded = rcu_access(_upgrade_futexes); \
+	if (_upgraded) { \
+		int _acquired = futex_trydown((fut)); \
+		if (!_acquired) { \
+			rcu_read_unlock(); \
+		} else { \
+			(fut)->upgraded = true; \
+		} \
+		_acquired; \
+	} else { \
+		(fut)->upgraded = false; \
+		1; \
+	} \
+})
+
+#define futex_unlock(fut) \
+({ \
+	if ((fut)->upgraded) \
+		(void) futex_up((fut)); \
+	rcu_read_unlock(); \
+})
+
+#define futex_give_to(fut, owner) ((void)0)
+#define futex_assert_is_locked(fut) ((void)0)
+#define futex_assert_is_not_locked(fut) ((void)0)
+
+extern int _upgrade_futexes;
+
+extern void futex_upgrade_all_and_wait(void);
+
+#elif FUTEX_DEBUG
+
+#define FUTEX_INITIALIZE(val) {{ (val) }, NULL }
+
+static inline void __futex_assert_is_locked(futex_t *futex, const char *name)
+{
+	void *owner = __atomic_load_n(&futex->owner, __ATOMIC_RELAXED);
+	fibril_t *self = (fibril_t *) fibril_get_id();
+	if (owner != self) {
+		__FUTEX_DPRINTF("Assertion failed: %s (%p) is not locked by fibril %p (instead locked by fibril %p).\n", name, futex, self, owner);
+	}
+	assert(owner == self);
+}
+
+static inline void __futex_assert_is_not_locked(futex_t *futex, const char *name)
+{
+	void *owner = __atomic_load_n(&futex->owner, __ATOMIC_RELAXED);
+	fibril_t *self = (fibril_t *) fibril_get_id();
+	if (owner == self) {
+		__FUTEX_DPRINTF("Assertion failed: %s (%p) is already locked by fibril %p.\n", name, futex, self);
+	}
+	assert(owner != self);
+}
+
+static inline void __futex_lock(futex_t *futex, const char *name)
+{
+	/* We use relaxed atomics to avoid violating C11 memory model.
+	 * They should compile to regular load/stores, but simple assignments
+	 * would be UB by definition.
+	 */
+
+	fibril_t *self = (fibril_t *) fibril_get_id();
+	__FUTEX_DPRINTF("Locking futex %s (%p) by fibril %p.\n", name, futex, self);
+	__futex_assert_is_not_locked(futex, name);
+	futex_down(futex);
+
+	void *prev_owner = __atomic_exchange_n(&futex->owner, self, __ATOMIC_RELAXED);
+	assert(prev_owner == NULL);
+
+	atomic_inc(&self->futex_locks);
+}
+
+static inline void __futex_unlock(futex_t *futex, const char *name)
+{
+	fibril_t *self = (fibril_t *) fibril_get_id();
+	__FUTEX_DPRINTF("Unlocking futex %s (%p) by fibril %p.\n", name, futex, self);
+	__futex_assert_is_locked(futex, name);
+	__atomic_store_n(&futex->owner, NULL, __ATOMIC_RELAXED);
+	atomic_dec(&self->futex_locks);
+	futex_up(futex);
+}
+
+static inline bool __futex_trylock(futex_t *futex, const char *name)
+{
+	fibril_t *self = (fibril_t *) fibril_get_id();
+	bool success = futex_trydown(futex);
+	if (success) {
+		void *owner = __atomic_load_n(&futex->owner, __ATOMIC_RELAXED);
+		assert(owner == NULL);
+
+		__atomic_store_n(&futex->owner, self, __ATOMIC_RELAXED);
+
+		atomic_inc(&self->futex_locks);
+
+		__FUTEX_DPRINTF("Trylock on futex %s (%p) by fibril %p succeeded.\n", name, futex, self);
+	} else {
+		__FUTEX_DPRINTF("Trylock on futex %s (%p) by fibril %p failed.\n", name, futex, self);
+	}
+
+	return success;
+}
+
+static inline void __futex_give_to(futex_t *futex, fibril_t *new_owner, const char *name)
+{
+	fibril_t *self = (fibril_t *) fibril_get_id();
+	__FUTEX_DPRINTF("Passing futex %s (%p) from fibril %p to fibril %p.\n", name, futex, self, new_owner);
+
+	__futex_assert_is_locked(futex, name);
+	atomic_dec(&self->futex_locks);
+	atomic_inc(&new_owner->futex_locks);
+	__atomic_store_n(&futex->owner, new_owner, __ATOMIC_RELAXED);
+}
+
+#define futex_lock(futex) __futex_lock((futex), #futex)
+#define futex_unlock(futex) __futex_unlock((futex), #futex)
+#define futex_trylock(futex) __futex_trylock((futex), #futex)
+#define futex_give_to(futex, new_owner) __futex_give_to((futex), (new_owner), #futex)
+#define futex_assert_is_locked(futex) __futex_assert_is_locked((futex), #futex)
+#define futex_assert_is_not_locked(futex) __futex_assert_is_not_locked((futex), #futex)
+
+#else
+
+#define FUTEX_INITIALIZE(val) {{ (val) }}
+
+#define futex_lock(fut)     (void) futex_down((fut))
+#define futex_trylock(fut)  futex_trydown((fut))
+#define futex_unlock(fut)   (void) futex_up((fut))
+#define futex_give_to(fut, owner) ((void)0)
+#define futex_assert_is_locked(fut) ((void)0)
+#define futex_assert_is_not_locked(fut) ((void)0)
+
+#endif
 
 #endif
 
