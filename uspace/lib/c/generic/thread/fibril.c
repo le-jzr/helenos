6232e6a13446636404c9318ea3e1c308e2191c11
Jiří Zárevúcky <jiri.zarevucky@nic.cz>
2018-07-09 20:21:41 +0200

    wip

--- uspace/lib/c/generic/thread/fibril.c
+++ uspace/lib/c/generic/thread/fibril.c
@@ -714,6 +714,7 @@ errno_t fibril_wait_timeout(fibril_event_t *event,
 
 	dstf = _ready_list_pop_nonblocking(true);
 	if (!dstf) {
+		#if 0
 		// XXX: It is possible for the _ready_list_pop_nonblocking() to
 		//      check for IPC, find a pending message, and trigger the
 		//      event on which we are currently trying to sleep.
@@ -722,6 +723,7 @@ errno_t fibril_wait_timeout(fibril_event_t *event,
 			futex_unlock(&fibril_futex);
 			return EOK;
 		}
+		#endif
 
 		dstf = srcf->thread_ctx;
 		assert(dstf);
