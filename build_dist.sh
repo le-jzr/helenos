10359a99954361811b30d2455d75e45d3940d7e1
Jiří Zárevúcky <zarevucky.jiri@gmail.com>
2019-06-09 19:49:28 +0200

    Install headers

--- build_dist.sh
+++ build_dist.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
 # Install generated map files into the 'debug' subdirectory.
+
 cd ${MESON_BUILD_ROOT}/uspace
 find -name '*.map' | xargs -I'@' install -C -D -m644 -T '@' "${DESTDIR}/${MESON_INSTALL_PREFIX}/debug/"'@'
+cd -
+
+# Install library headers that are mixed in with source files (no separate 'include' subdir).
+# The properly separated headers are installed by the meson script.
+
+cd ${MESON_SOURCE_ROOT}/uspace/lib
+
+incdir="${DESTDIR}/${MESON_INSTALL_PREFIX}/include"
+
+for lib in *; do
+	if [ -d ${lib} -a ! -d ${lib}/include ]; then
+		cd ${lib} || continue
+		mkdir -p "${incdir}/lib${lib}"
+		find -name '*.h' -a '!' -path './doc/*' | xargs -I'@' install -C -D -m644 -T '@' "${incdir}/lib${lib}/"'@'
+		cd -
+	fi
+done
