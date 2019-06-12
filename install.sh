41ea33dad47ed79c50bce5d8101bbd5631e9bd0a
Jiří Zárevúcky <zarevucky.jiri@gmail.com>
2019-06-12 17:10:01 +0200

    Simplify executable installation

--- install.sh
+++ install.sh
@@ -22,42 +22,15 @@
 		fi
 	done
 
-	# Due to certain quirks of our build, executables are built with a different name than what they are installed with.
+	# Due to certain quirks of our build, executables need to be built with a different name than what they are installed with.
 	# Meson doesn't support renaming installed files (at least not as of mid-2019) so we do it here manually.
 
 	echo "######## Installing executables ########"
 
-	cd ${MESON_SOURCE_ROOT}/uspace
+	cd ${MESON_BUILD_ROOT}/uspace
 
-	for subdir in app drv srv; do
-		# Get list of Meson subdirectories.
-		dirs=`find -name meson.build -a -path ./${subdir}'/*/*' | sed 's/^\.\/\(.*\)\/meson.build$/\1/'`
-
-		for dir in $dirs; do
-			underscored=`echo $dir | sed s:[/-]:_:g`
-			testname=`dirname $dir`/test-`basename $dir`
-			testname_underscored=`echo $testname | sed s:[/-]:_:g`
-
-			if [ -f ${MESON_BUILD_ROOT}/uspace/${underscored} ]; then
-				echo | xargs --verbose install -C -D -m755 -T ${MESON_BUILD_ROOT}/uspace/${underscored}        ${MESON_INSTALL_DESTDIR_PREFIX}${dir}
-			fi
-			if [ -f ${MESON_BUILD_ROOT}/uspace/${underscored}.map ]; then
-				echo | xargs --verbose install -C -D -m755 -T ${MESON_BUILD_ROOT}/uspace/${underscored}.map    ${MESON_INSTALL_DESTDIR_PREFIX}debug/${dir}.map
-			fi
-			if [ -f ${MESON_BUILD_ROOT}/uspace/${underscored}.disasm ]; then
-				echo | xargs --verbose install -C -D -m755 -T ${MESON_BUILD_ROOT}/uspace/${underscored}.disasm ${MESON_INSTALL_DESTDIR_PREFIX}debug/${dir}.disasm
-			fi
-
-			if [ -f ${MESON_BUILD_ROOT}/uspace/${testname_underscored} ]; then
-				echo | xargs --verbose install -C -D -m755 -T ${MESON_BUILD_ROOT}/uspace/${testname_underscored}        ${MESON_INSTALL_DESTDIR_PREFIX}test/${testname}
-			fi
-			if [ -f ${MESON_BUILD_ROOT}/uspace/${testname_underscored}.map ]; then
-				echo | xargs --verbose install -C -D -m755 -T ${MESON_BUILD_ROOT}/uspace/${testname_underscored}.map    ${MESON_INSTALL_DESTDIR_PREFIX}debug/test/${testname}.map
-			fi
-			if [ -f ${MESON_BUILD_ROOT}/uspace/${testname_underscored}.disasm ]; then
-				echo | xargs --verbose install -C -D -m755 -T ${MESON_BUILD_ROOT}/uspace/${testname_underscored}.disasm ${MESON_INSTALL_DESTDIR_PREFIX}debug/test/${testname}.disasm
-			fi
-		done
-	done
+	find -name 'install@*' |
+		sed -e 'h; s:^.*/install@:@DESTDIR@:; s:\$:/:g; x; G; s:\s: :g' -e "s:@DESTDIR@:${MESON_INSTALL_DESTDIR_PREFIX}:g" |
+		xargs -L1 --verbose install -C -D -m755 -T
 
 ) > ${MESON_BUILD_ROOT}/install_custom.log 2>&1
