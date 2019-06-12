74b66f42bae2fee91d9f4d96d6ec1f8fbeb8f3b8
Jiří Zárevúcky <zarevucky.jiri@gmail.com>
2019-06-12 18:20:14 +0200

    Make amd64 image buildable again

--- install.sh
+++ install.sh
@@ -3,16 +3,20 @@
 (
 	# Install generated map files into the 'debug' subdirectory.
 
-	echo "######## Installing map files ########"
+	echo "######## Installing library map files ########"
+
+	# TODO: only install when enabled
 
 	cd ${MESON_BUILD_ROOT}/uspace
-	find -name '*.map' -a -path './*/*' | sed 's:^\./::' | xargs --verbose -I'@' install -C -D -m644 -T '@' "${MESON_INSTALL_DESTDIR_PREFIX}debug/"'@'
+	find -name '*.map' -a -path './lib/*' | sed 's:^\./::' | xargs --verbose -I'@' install -C -D -m644 -T '@' "${MESON_INSTALL_DESTDIR_PREFIX}debug/"'@'
 
 	# Install library headers that are mixed in with source files (no separate 'include' subdir).
 	# The properly separated headers are installed by the meson script.
 
 	echo "######## Installing headers ########"
 	cd ${MESON_SOURCE_ROOT}/uspace
+
+	# TODO: only install when enabled
 
 	incdir="${MESON_INSTALL_DESTDIR_PREFIX}include"
 
