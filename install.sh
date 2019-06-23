ab2f00841893dca6a7b1e34bcd4169586d6e566f
Jiří Zárevúcky <zarevucky.jiri@gmail.com>
2019-06-23 14:29:36 +0200

    Only install headers if configuration allows

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+export CONFIG_DEVEL_FILES=$1
+
 (
 	# Install generated map files into the 'debug' subdirectory.
 
@@ -15,18 +17,18 @@
 	# Install library headers that are mixed in with source files (no separate 'include' subdir).
 	# The properly separated headers are installed by the meson script.
 
-	echo "######## Installing headers ########"
-	cd ${MESON_SOURCE_ROOT}/uspace
+	if $CONFIG_DEVEL_FILES; then
+		echo "######## Installing headers ########"
+		cd ${MESON_SOURCE_ROOT}/uspace
 
-	# TODO: only install when enabled
+		incdir="${MESON_INSTALL_DESTDIR_PREFIX}include"
 
-	incdir="${MESON_INSTALL_DESTDIR_PREFIX}include"
-
-	for libdir in lib/*; do
-		if [ -d ${libdir} -a ! -d ${libdir}/include ]; then
-			find ${libdir} -maxdepth 1 -name '*.h' -a '!' -path ${libdir}'/doc/*' | sed 's:^lib/::' | xargs --verbose -I'@' install -C -D -m644 -T 'lib/@' ${incdir}'/lib@'
-		fi
-	done
+		for libdir in lib/*; do
+			if [ -d ${libdir} -a ! -d ${libdir}/include ]; then
+				find ${libdir} -maxdepth 1 -name '*.h' -a '!' -path ${libdir}'/doc/*' | sed 's:^lib/::' | xargs --verbose -I'@' install -C -D -m644 -T 'lib/@' ${incdir}'/lib@'
+			fi
+		done
+	fi
 
 	# Due to certain quirks of our build, executables need to be built with a different name than what they are installed with.
 	# Meson doesn't support renaming installed files (at least not as of mid-2019) so we do it here manually.
