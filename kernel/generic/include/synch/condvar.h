6cfeea5e66fb5f506c5987ab27605be979cee68b
Jiří Zárevúcky <zarevucky.jiri@gmail.com>
2022-08-18 17:57:35 +0200

    Remove unused spinlock condvar

--- kernel/generic/include/synch/condvar.h
+++ kernel/generic/include/synch/condvar.h
@@ -45,25 +45,12 @@ typedef struct {
 	waitq_t wq;
 } condvar_t;
 
-#ifdef CONFIG_SMP
-#define _condvar_wait_timeout_spinlock(cv, lock, usec, flags) \
-	_condvar_wait_timeout_spinlock_impl((cv), (lock), (usec), (flags))
-#else
-#define _condvar_wait_timeout_spinlock(cv, lock, usec, flags) \
-	_condvar_wait_timeout_spinlock_impl((cv), NULL, (usec), (flags))
-#endif
-
 extern void condvar_initialize(condvar_t *cv);
 extern void condvar_signal(condvar_t *cv);
 extern void condvar_broadcast(condvar_t *cv);
 
 extern errno_t condvar_wait(condvar_t *cv, mutex_t *mtx);
 extern errno_t condvar_wait_timeout(condvar_t *cv, mutex_t *mtx, uint32_t usec);
-
-extern errno_t _condvar_wait_timeout_spinlock_impl(condvar_t *cv, spinlock_t *lock,
-    uint32_t usec, int flags);
-extern errno_t _condvar_wait_timeout_irq_spinlock(condvar_t *cv,
-    irq_spinlock_t *irq_lock, uint32_t usec, int flags);
 
 #endif
 
